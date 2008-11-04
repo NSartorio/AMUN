e6ae8fd2b0fbe680b284ee1b3783ebfd5a8d8804
Grzegorz Kowal <grzegorz@gkowal.info>
2008-11-04 13:08:01 -0600

    Initial commit of Godunov-AMR code.
    Initial commit includes driver, make system files, license and default
    host configurations.

---
+++ src/driver.F90
@@ -0,0 +1,45 @@
+!!**********************************************************************
+!!
+!! program: Godunov-AMR
+!!
+!! Copyright (C) 2008 Grzegorz Kowal <kowal@astro.wisc.edu>
+!!
+!!******************************************************************************
+!!
+!!  This file is part of Godunov-AMR.
+!!
+!!  Godunov-AMR is free software; you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License as published by
+!!  the Free Software Foundation; either version 3 of the License, or
+!!  (at your option) any later version.
+!!
+!!  Godunov-AMR is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!!
+!!******************************************************************************
+!!
+!
+program godunov
+
+!
+!----------------------------------------------------------------------
+!
+! local variables
+!
+!
+!----------------------------------------------------------------------
+!
+  write (*,"(1x,78('-'))")
+  write (*,"(1x,18('='),4x,a,4x,19('='))") '      Godunov-AMR algorithm      '
+  write (*,"(1x,18('='),4x,a,4x,19('='))") 'Copyright (C) 2008 Grzegorz Kowal'
+  write (*,"(1x,78('-'))")
+  write (*,*)
+
+!----------------------------------------------------------------------
+!
+end program
