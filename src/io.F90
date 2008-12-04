5187f4942de377580b6e8b377774690422859ca5
Grzegorz Kowal <grzegorz@gkowal.info>
2008-12-03 23:36:07 -0600

    We can write some of the block attributes now.
    I've implemented storing some of the block attributes in HDF5 files. I
    shall complete this by writing down neighbors and children. Then I shall
    implement storing data arrays.

--- src/io.F90
+++ src/io.F90
@@ -39,9 +39,13 @@ module io
 !
   subroutine write_data(ftype, nfile, nproc)
 
+    use blocks, only : block, pfirst
     use error , only : print_error
-    use hdf5  , only : h5open_f, h5close_f, h5fcreate_f, h5fclose_f  &
-                     , hid_t, H5F_ACC_TRUNC_F
+    use hdf5  , only : h5open_f, h5close_f, h5fcreate_f, h5fclose_f         &
+                     , h5gcreate_f, h5gclose_f, h5acreate_f, h5aclose_f     &
+                     , h5awrite_f, h5screate_simple_f, h5sclose_f           &
+                     , hid_t, hsize_t, H5F_ACC_TRUNC_F                      &
+                     , H5T_NATIVE_INTEGER, H5T_NATIVE_DOUBLE
 
     implicit none
 
@@ -52,13 +56,17 @@ module io
 
 ! HDF5 variables
 !
-    integer(hid_t)    :: fid
-
+    integer(hid_t)    :: fid, gid, sid, aid
+    integer(hsize_t)  :: am(1)
 
 ! local variables
 !
-    character(len=64) :: fl
+    character(len=64) :: fl, gnm
     integer           :: err
+
+! pointers
+!
+    type(block), pointer :: pcurr, ptemp
 !
 !----------------------------------------------------------------------
 !
@@ -82,6 +90,79 @@ module io
 ! TODO: write coordinates for all refinement levels
 ! TODO: iterate over all blocks and write complete structure of each of them
 !
+        pcurr => pfirst
+
+        do while(associated(pcurr))
+
+          write(gnm,"('blk',i8.8)") pcurr%id
+
+          call h5gcreate_f(fid, gnm, gid, err)
+
+! TODO: create block attributes (xmin, xmax, ..., level, parent, all entries of structure)
+!
+          am(1) = 1
+          call h5screate_simple_f(1, am, sid, err)
+
+          call h5acreate_f(gid, 'id', H5T_NATIVE_INTEGER, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_INTEGER, pcurr%id, am, err)
+          call h5aclose_f(aid, err)
+
+          ptemp => pcurr%prev
+          call h5acreate_f(gid, 'prev', H5T_NATIVE_INTEGER, sid, aid, err)
+          if (associated(ptemp)) then
+            call h5awrite_f(aid, H5T_NATIVE_INTEGER, ptemp%id, am, err)
+          else
+            call h5awrite_f(aid, H5T_NATIVE_INTEGER, -1, am, err)
+          endif
+          call h5aclose_f(aid, err)
+
+          ptemp => pcurr%next
+          call h5acreate_f(gid, 'next', H5T_NATIVE_INTEGER, sid, aid, err)
+          if (associated(ptemp)) then
+            call h5awrite_f(aid, H5T_NATIVE_INTEGER, ptemp%id, am, err)
+          else
+            call h5awrite_f(aid, H5T_NATIVE_INTEGER, -1, am, err)
+          endif
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'parent', H5T_NATIVE_INTEGER, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_INTEGER, pcurr%parent, am, err)
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'level', H5T_NATIVE_INTEGER, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_INTEGER, pcurr%level, am, err)
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'xmin', H5T_NATIVE_DOUBLE, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_DOUBLE, real(pcurr%xmin,8), am, err)
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'xmax', H5T_NATIVE_DOUBLE, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_DOUBLE, real(pcurr%xmax,8), am, err)
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'ymin', H5T_NATIVE_DOUBLE, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_DOUBLE, real(pcurr%ymin,8), am, err)
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'ymax', H5T_NATIVE_DOUBLE, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_DOUBLE, real(pcurr%ymax,8), am, err)
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'zmin', H5T_NATIVE_DOUBLE, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_DOUBLE, real(pcurr%zmin,8), am, err)
+          call h5aclose_f(aid, err)
+
+          call h5acreate_f(gid, 'zmax', H5T_NATIVE_DOUBLE, sid, aid, err)
+          call h5awrite_f(aid, H5T_NATIVE_DOUBLE, real(pcurr%zmax,8), am, err)
+          call h5aclose_f(aid, err)
+
+          call h5sclose_f(sid, err)
+
+          call h5gclose_f(gid, err)
+
+          pcurr => pcurr.next
+        end do
 
 ! terminate access to the file
 !
