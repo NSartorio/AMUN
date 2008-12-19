d67f05d65018220976b3f791ed80771fb3913504
Grzegorz Kowal <grzegorz@gkowal.info>
2008-12-19 17:33:29 -0600

    Add a new module for MPI communication.

---
+++ src/mpitools.F90
@@ -0,0 +1,36 @@
+!!*****************************************************************************
+!!
+!! module: mpitools - subroutines for MPI communication
+!!
+!! Copyright (C) 2008 Grzegorz Kowal <kowal@astro.wisc.edu>
+!!
+!!*****************************************************************************
+!!
+!!  This file is part of Godunov-AMR.
+!!
+!!  Godunov-AMR is free software; you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License as published by
+!!  the Free Software Foundation; either version 3 of the License, or
+!!  (at your option) any later version.
+!!
+!!  Godunov-AMR is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!!
+!!*****************************************************************************
+!!
+!
+module mpitools
+
+  implicit none
+
+! MPI global variables
+!
+  integer(kind=4), save                 :: comm3d
+  integer(kind=4), save                 :: nproc, nprocs
+
+end module
