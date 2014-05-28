9b22d2be7b27d485eba9c64eaad63ddb66b5bc8d
Grzegorz Kowal <grzegorz@amuncode.org>
2014-05-28 18:09:35 -0300

    OPERATORS: Add new module for differential operators.
    Signed-off-by: Grzegorz Kowal <grzegorz@amuncode.org>

---
+++ src/operators.F90
@@ -0,0 +1,43 @@
+!!******************************************************************************
+!!
+!!  This file is part of the AMUN source code, a program to perform
+!!  Newtonian or relativistic magnetohydrodynamical simulations on uniform or
+!!  adaptive mesh.
+!!
+!!  Copyright (C) 2008-2014 Grzegorz Kowal <grzegorz@amuncode.org>
+!!
+!!  This program is free software: you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License as published by
+!!  the Free Software Foundation, either version 3 of the License, or
+!!  (at your option) any later version.
+!!
+!!  This program is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!!
+!!*****************************************************************************
+!!
+!! module: OPERATORS
+!!
+!!  This module provides differential operators like gradient, divergence, or
+!!  curl.
+!!
+!!*****************************************************************************
+!
+module operators
+
+! module variables are not implicit by default
+!
+  implicit none
+
+! by default everything is public
+!
+  private
+
+!===============================================================================
+!
+end module operators
