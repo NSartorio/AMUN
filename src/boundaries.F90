d4f43f2ebe9fd9d0f4cd2f75db3d1f62b98efbc9
Grzegorz Kowal <grzegorz@amuncode.org>
2014-12-11 19:21:37 -0200

    BOUNDARIES: Rewrite MPI part in boundaries_corner_prolong().
    We use exchange_real_arrays() now.
    
    Signed-off-by: Grzegorz Kowal <grzegorz@amuncode.org>

--- src/boundaries.F90
+++ src/boundaries.F90
@@ -5053,10 +5053,9 @@ module boundaries
     use coordinates    , only : ibl, jbl, kbl
     use coordinates    , only : ieu, jeu, keu
     use equations      , only : nv
-    use mpitools       , only : nproc, nprocs, npmax
 #ifdef MPI
-    use mpitools       , only : npairs, pairs
-    use mpitools       , only : send_real_array, receive_real_array
+    use mpitools       , only : nproc, nprocs, npairs, pairs
+    use mpitools       , only : exchange_real_arrays
 #endif /* MPI */
 
 ! local variables are not implicit by default
@@ -5075,14 +5074,14 @@ module boundaries
     integer :: i , j , k
     integer :: il, jl, kl
     integer :: iu, ju, ku
-    integer :: iret
 #ifdef MPI
-    integer :: isend, irecv, nblocks, itag
-    integer :: l, p
+    integer :: sproc, scount, stag
+    integer :: rproc, rcount, rtag
+    integer :: l, p, iret
 
 ! local arrays
 !
-    real(kind=8), dimension(:,:,:,:,:), allocatable :: rbuf
+    real(kind=8), dimension(:,:,:,:,:), allocatable :: sbuf, rbuf
 #endif /* MPI */
 !
 !-------------------------------------------------------------------------------
@@ -5232,45 +5231,57 @@ module boundaries
 !!
 ! iterate over all process pairs
 !
-    do p = 1, 2 * npairs
+    do p = 1, npairs
 
-! get sending and receiving process identifiers
+! process only pairs related to this process
 !
-      isend = pairs(p,1)
-      irecv = pairs(p,2)
+      if (pairs(p,1) == nproc .or. pairs(p,2) == nproc) then
 
-! process only pairs which have something to exchange
+! get sending and receiving process identifiers (depending on pair member)
 !
-      if (bcount(isend,irecv) > 0) then
+        if (pairs(p,1) == nproc) then
+          sproc = pairs(p,1)
+          rproc = pairs(p,2)
+        end if
+        if (pairs(p,2) == nproc) then
+          sproc = pairs(p,2)
+          rproc = pairs(p,1)
+        end if
 
-! obtain the number of blocks to exchange
+! get the number of blocks to exchange
 !
-        nblocks = bcount(isend,irecv)
+        scount = bcount(sproc,rproc)
+        rcount = bcount(rproc,sproc)
+
+! process only pairs which have anything to exchange
+!
+        if ((scount + rcount) > 0) then
 
 ! prepare the tag for communication
 !
-        itag = 16 * (irecv * nprocs + isend) + 10
+          stag = 16 * (rproc * nprocs + sproc) + 10
+          rtag = 16 * (sproc * nprocs + rproc) + 10
 
-! allocate data buffer for variables to exchange
+! allocate buffers for variable exchange
 !
 #if NDIMS == 2
-        allocate(rbuf(nblocks,nv,ng,ng,km))
+          allocate(sbuf(scount,nv,ng,ng,km))
+          allocate(rbuf(rcount,nv,ng,ng,km))
 #endif /* NDIMS == 2 */
 #if NDIMS == 3
-        allocate(rbuf(nblocks,nv,ng,ng,ng))
+          allocate(sbuf(scount,nv,ng,ng,ng))
+          allocate(rbuf(rcount,nv,ng,ng,ng))
 #endif /* NDIMS == 3 */
 
-! if isend == nproc we are sending data from the neighbor block
-!
-        if (isend == nproc) then
-
+!! PREPARE BLOCKS FOR SENDING
+!!
 ! reset the block counter
 !
           l = 0
 
 ! associate the pointer with the first block in the exchange list
 !
-          pinfo => barray(isend,irecv)%ptr
+          pinfo => barray(sproc,rproc)%ptr
 
 ! scan over all blocks on the block exchange list
 !
@@ -5292,18 +5303,18 @@ module boundaries
             k = pinfo%corner(3)
 #endif /* NDIMS == 3 */
 
-! restrict and extract the corresponding corner region from the neighbor and
-! insert it to the buffer
+! prolong the corresponding corner region from the neighbor and insert it in
+! the buffer
 !
 #if NDIMS == 2
             call block_corner_prolong(i, j, k                                  &
                                    , pneigh%data%q(1:nv,1:im,1:jm,1:km)        &
-                                   ,        rbuf(l,1:nv,1:ng,1:ng,1:km))
+                                   ,        sbuf(l,1:nv,1:ng,1:ng,1:km))
 #endif /* NDIMS == 2 */
 #if NDIMS == 3
             call block_corner_prolong(i, j, k                                  &
                                    , pneigh%data%q(1:nv,1:im,1:jm,1:km)        &
-                                   ,        rbuf(l,1:nv,1:ng,1:ng,1:ng))
+                                   ,        sbuf(l,1:nv,1:ng,1:ng,1:ng))
 #endif /* NDIMS == 3 */
 
 ! associate the pointer with the next block
@@ -5312,28 +5323,22 @@ module boundaries
 
           end do ! %ptr block list
 
-! send the data buffer to another process
+!! SEND PREPARED BLOCKS AND RECEIVCE NEW ONES
+!!
+! exchange data
 !
-          call send_real_array(size(rbuf), irecv, itag, rbuf(:,:,:,:,:), iret)
+          call exchange_real_arrays(rproc, stag, size(sbuf), sbuf              &
+                                  , rproc, rtag, size(rbuf), rbuf, iret)
 
-        end if ! isend = nproc
-
-! if irecv == nproc we are receiving data from the neighbor block
-!
-        if (irecv == nproc) then
-
-! receive the data buffer
-!
-          call receive_real_array(size(rbuf(:,:,:,:,:)), isend, itag           &
-                                                      , rbuf(:,:,:,:,:), iret)
-
+!! PROCESS RECEIVED BLOCKS
+!!
 ! reset the block counter
 !
           l = 0
 
 ! associate the pointer with the first block in the exchange list
 !
-          pinfo => barray(isend,irecv)%ptr
+          pinfo => barray(rproc,sproc)%ptr
 
 ! iterate over all received blocks and update boundaries of the corresponding
 ! data blocks
@@ -5397,13 +5402,13 @@ module boundaries
 
           end do ! %ptr block list
 
-        end if ! irecv = nproc
-
 ! deallocate data buffer
 !
-        if (allocated(rbuf)) deallocate(rbuf)
+          deallocate(sbuf, rbuf)
 
-      end if ! if bcount > 0
+        end if ! (scount + rcount) > 0
+
+      end if ! pairs(p,1) == nproc || pairs(p,2) == nproc
 
     end do ! p = 1, npairs
 
