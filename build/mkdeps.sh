ed38be7ebf6d115e0e64a406ad1c18c274fad0e3
Grzegorz Kowal <grzegorz@amuncode.org>
2018-03-30 21:31:36 -0300

    Make mkdeps.sh script more robust.
    Get rid of 'grep' and use only 'awk', 'sort' and 'uniq'. All should be
    available in any linux system.
    
    Signed-off-by: Grzegorz Kowal <grzegorz@amuncode.org>

--- build/mkdeps.sh
+++ build/mkdeps.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 #
+# A script to generate compile dependencies for Fortran files.
+#
+# Usage:
+#
+#   mkdep.sh (FORTRAN_SOURCE_FILE_PATH) (OBJECT_FILE_PATH)
+#
+# Example:
+#
+#   mkdep.sh ../src ./obj
+#
+# Requirements: awk, sort, uniq
+#
 files=`ls $1/*.F90`
 for src in $files; do
-  deps=`grep -i '  use ' $src | awk '{ print $2 }' | sed 's/,//g' | sort | uniq`
+  deps=`awk '/^\s*[Uu][Ss][Ee] / {gsub ( "[:,]","" ) ; print $2}' $src | sort | uniq`
   fname=`basename $src .F90`
   output="$2/${fname}.o: $src"
   for dep in $deps; do
